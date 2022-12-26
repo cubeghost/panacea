e803d2648804a843c67a9ae247e89722d48e9fb7
alex baldwin <ghostbaldwin@gmail.com>
2022-12-25 22:59:52 -0500

    basic calendar

---
+++ app/components/Calendar.tsx
@@ -0,0 +1,35 @@
+
+import cn from 'classnames';
+import {eachDayOfInterval, startOfMonth, endOfMonth, isToday, getDate, getDay} from 'date-fns'
+
+const Calendar = () => {
+  const now = new Date();
+
+  const days = eachDayOfInterval({
+    start: startOfMonth(now),
+    end: endOfMonth(now),
+  });
+
+  return (
+    <div className="month">
+      {days.map((day) => {
+        const date = getDate(day);
+        return (
+          <div
+            className={cn('day', {
+              isToday: isToday(day),
+            })}
+            style={{
+              gridColumn: getDay(day),
+            }}
+            key={date}
+          >
+            {date}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Calendar;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,15 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 import type { User } from '@prisma/client';
 
 import { auth } from '~/services/auth.server';
+import styles from '~/styles/calendar.css';
+import Calendar from '~/components/Calendar';
+
+export function links() {
+  return [ { rel: 'stylesheet', href: styles } ];
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await auth.isAuthenticated(request, { failureRedirect: '/login' })
@@ -15,6 +21,10 @@ export default function Index() {
   return (
     <div>
       hi {user.email}
+      <br />
+      <div style={{maxWidth: '300px'}}>
+      <Calendar />
+      </div>
     </div>
   );
 }
